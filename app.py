3915a50b054716ae9eb0c95e833fede0d7de5b3b
RayenMnif <mnif.rayen07@gmail.com>
2024-03-29 23:22:05 +0100

    working on the menu

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ class Player(pygame.sprite.Sprite):
         self.gravity = 0
         # player 
         self.image = self.frames[self.animation_index]
-        self.rect = self.image.get_rect(center = (80, 100))
+        self.rect = self.image.get_rect(center = (140, 360)) 
     def animations(self):
         self.animation_index += 0.1
         if self.animation_index >= len(self.frames): self.animation_index = 0
@@ -31,8 +31,10 @@ class Player(pygame.sprite.Sprite):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE] and self.rect.top <= 490: self.gravity = -7 
     def update(self):
-        self.physics()
-        self.jump()
+        if game_active:
+            self.physics()
+            self.jump()
+        if not game_active: self.rect.center = (140, 360)
         self.animations()
 
 class Obstacles(pygame.sprite.Sprite):
@@ -47,11 +49,18 @@ class Obstacles(pygame.sprite.Sprite):
         self.rect.x -= 5
         if self.rect.x <= -200: self.kill() 
 
+def get_score():
+    return 
+
 pygame.init()
 
 screen = pygame.display.set_mode((280, 510))
 pygame.display.set_caption("asfour stah")
 clock = pygame.time.Clock()
+
+game_active = False
+
+font = pygame.font.Font("FlappyBirdy.ttf", 12)
 
 # game image
 bg_image = pygame.image.load("sprites/background-day.png").convert_alpha()
@@ -70,19 +79,30 @@ pygame.time.set_timer(obstacles_timer, 800)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: pygame.quit()
-        if event.type == obstacles_timer: 
-            if randint(0,1):
-                obstacles.add(Obstacles("normal"))
-            else:
-                obstacles.add(Obstacles("down"))
+        if game_active:
+            if event.type == obstacles_timer: 
+                if randint(0,1):
+                    obstacles.add(Obstacles("normal"))
+                else:
+                    obstacles.add(Obstacles("down"))
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+        """"if event.type == pygame.MOUSEBUTTONDOWN:
+            print(pygame.mouse.get_pos()) # for debugging """
 
     screen.blit(bg_image, (0,0))
 
     player.draw(screen)
     player.update()
-
-    obstacles.draw(screen)
-    obstacles.update()
+    
+    if game_active:
+        obstacles.draw(screen)
+        obstacles.update()
+        game_active = not (pygame.sprite.spritecollide(player.sprite, obstacles, False))
+    else:
+        obstacles.empty()
+        screen.blit(pygame.image.load("sprites/message.png").convert_alpha(),pygame.image.load("sprites/message.png").get_rect(center = (140,255)))
 
     screen.blit(ground_image, (0, 450))
 
