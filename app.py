4d82c150af4f7f10603c943cdeb7b730529e6b3b
RayenMnif <mnif.rayen07@gmail.com>
2024-03-29 16:04:04 +0100

    adding obstacle sprite and a time for osbtcles spawning

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 from sys import exit
 
 
@@ -15,7 +16,7 @@ class Player(pygame.sprite.Sprite):
         self.gravity = 0
         # player 
         self.image = self.frames[self.animation_index]
-        self.rect = self.image.get_rect(center = (140, 5))
+        self.rect = self.image.get_rect(center = (140, 0))
     def animations(self):
         self.animation_index += 0.1
         if self.animation_index >= len(self.frames): self.animation_index = 0
@@ -25,11 +26,20 @@ class Player(pygame.sprite.Sprite):
         self.rect.y += self.gravity
     def jump(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE] and self.rect.top <= 620: self.gravity = -7 
+        if keys[pygame.K_SPACE] and self.rect.top <= 490: self.gravity = -7 
     def update(self):
         self.physics()
         self.jump()
         self.animations()
+
+class Obstacles(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("sprites/pipe-green.png")
+        self.rect = self.image.get_rect(midbottom = (randint(280, 350), randint(570, 700)))
+    def update(self):
+        self.rect.x -= 5
+        if self.rect.x <= -50: self.kill() 
 
 pygame.init()
 
@@ -44,6 +54,13 @@ bg_image = pygame.image.load("sprites/background-day.png")
 player = pygame.sprite.GroupSingle()
 player.add(Player())
 
+obstacles = pygame.sprite.GroupSingle()
+obstacles.add(Obstacles())
+
+# Obstacles timer
+obstacles_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacles_timer, 1000)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: pygame.quit()
@@ -52,6 +69,9 @@ while True:
 
     player.draw(screen)
     player.update()
+
+    obstacles.draw(screen)
+    obstacles.update()
 
     pygame.display.update()
 
