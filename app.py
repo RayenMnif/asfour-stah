4811400f0808a1b61c22fd7094ad265bfc71dedd
RayenMnif <mnif.rayen07@gmail.com>
2024-03-30 14:28:46 +0100

    adding a working audio system + score

--- app.py
+++ app.py
@@ -29,13 +29,17 @@ class Player(pygame.sprite.Sprite):
         self.rect.y += self.gravity
     def jump(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE] and self.rect.top <= 490: self.gravity = -6 
+        if keys[pygame.K_SPACE] and self.rect.top <= 490: 
+            self.gravity = -6 
+            wing_sound.play()
     def update(self):
         global game_active 
         if game_active:
             self.physics()
             self.jump()
-            if self.rect.y >= 450 : game_active = False
+            if self.rect.y >= 450 : 
+                hit_sound.play()
+                game_active = False
         if not game_active: self.rect.center = (140, 360)
         self.animations()
 
@@ -63,8 +67,14 @@ font = pygame.font.Font("font.otf", 16)
 
 score = 0
 
+# sound effect
+wing_sound = pygame.mixer.Sound("audio/action_jump.mp3")
+wing_sound.set_volume(0.3)
+hit_sound = pygame.mixer.Sound("audio/hit.mp3")
+point_sound = pygame.mixer.Sound("audio/point.mp3")
+
 # game image
-bg_image = pygame.image.load("sprites/background-day.png").convert_alpha()
+bg_image = pygame.image.load("sprites/background-day.png").convert_alpha() 
 ground_image = pygame.image.load("sprites/base.png").convert_alpha()
 
 # Groups
@@ -86,6 +96,7 @@ while True:
                     obstacles.add(Obstacles("normal", ypos))
                     obstacles.add(Obstacles("down", ypos))
                     score += 1
+                    if score > 0: point_sound.play()
         else:
             score = -1
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
@@ -97,18 +108,28 @@ while True:
     player.update()
 
     tmp_score = score
-    if tmp_score == -1 : tmp_score = 0
-    score_surf = font.render(f"score : {int(tmp_score)}", True, "black").convert_alpha()
-    score_rect = score_surf.get_rect(topleft = (5, 5)) 
 
     if game_active:
+        if tmp_score == -1 : tmp_score = 0
+        score_surf = font.render(f"score : {int(tmp_score)}", True, "black").convert_alpha()
+        score_rect = score_surf.get_rect(topleft = (5, 5)) 
         obstacles.draw(screen)
         obstacles.update()
         screen.blit(score_surf, score_rect)
-        game_active = not (pygame.sprite.spritecollide(player.sprite, obstacles, False))
+        if pygame.sprite.spritecollide(player.sprite, obstacles, False):
+            hit_sound.play()
+            game_active = False
     else:
         obstacles.empty()
         screen.blit(pygame.image.load("sprites/message.png").convert_alpha(),pygame.image.load("sprites/message.png").get_rect(center = (140,255)))
+        if tmp_score != -1:
+            score_surf = font.render("your last score", True, "black").convert_alpha()
+            score_surf_1 = font.render(f"{tmp_score}", True, "black").convert_alpha()
+            score_rect = score_surf.get_rect(center = (140, 415)) 
+            score_rect_1 = score_surf_1.get_rect(center = (140, 432)) 
+            screen.blit(score_surf, score_rect)
+            screen.blit(score_surf_1, score_rect_1)
+        
 
     screen.blit(ground_image, (0, 450))
 
